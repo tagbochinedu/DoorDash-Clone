5a5e92caeb39b6c940d9cf67b7058b6e42750038
Tagbo Chinedu <johntagbo2@gmail.com>
2022-08-02 19:56:49 +0100

    new commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,9 @@
 const Footer = () => {
   return (
-    <div className="bg-footer px-28 py-8 ">
-      
-        <div>
-          {" "}
-          <h4 className="text-gray-200 font-bold text-md">Top Cities</h4>
-          <div></div>
+    <div className="bg-footer px-28 py-8  flex justify-around">
+      <div className="md:w-5/12">
+        <h4 className="text-gray-200 font-bold text-md mb-3 ">Top Cities</h4>
+        <div className="flex justify-between">
           <ul>
             <li className="my-1.5">
               <a
@@ -138,12 +136,216 @@ const Footer = () => {
               </a>
             </li>
           </ul>
+          <ul>
+            {" "}
+            <li className="my-1.5">
+              <a
+                href="https://www.doordash.com/food-delivery/denver-co-restaurants/"
+                className="text-gray-300 text-sm no-underline  font-semibold hover:underline hover:text-white"
+              >
+                Denver
+              </a>
+            </li>
+            <li className="my-1.5">
+              <a
+                href="https://www.doordash.com/food-delivery/austin-tx-restaurants/"
+                className="text-gray-300 text-sm no-underline  font-semibold hover:underline hover:text-white"
+              >
+                Austin
+              </a>
+            </li>
+            <li className="my-1.5">
+              <a
+                href="https://www.doordash.com/food-delivery/washington-dc-dc-restaurants/"
+                className="text-gray-300 text-sm no-underline  font-semibold hover:underline hover:text-white"
+              >
+                Washington, DC
+              </a>
+            </li>
+            <li className="my-1.5">
+              <a
+                href="https://www.doordash.com/food-delivery/dallas-tx-restaurants/"
+                className="text-gray-300 text-sm no-underline  font-semibold hover:underline hover:text-white"
+              >
+                Dallas
+              </a>
+            </li>
+            <li className="my-1.5">
+              <a
+                href="https://www.doordash.com/food-delivery/melbourne-vic-restaurants/"
+                className="text-gray-300 text-sm no-underline  font-semibold hover:underline hover:text-white"
+              >
+                Melbourne
+              </a>
+            </li>
+            <li className="my-1.5">
+              <a
+                href="https://www.doordash.com/food-delivery/sydney-nsw-restaurants/"
+                className="text-gray-300 text-sm no-underline  font-semibold hover:underline hover:text-white"
+              >
+                Sydney
+              </a>
+            </li>
+            <li className="my-1.5">
+              <a
+                href="https://www.doordash.com/food-delivery/montreal-qc-restaurants/"
+                className="text-gray-300 text-sm no-underline  font-semibold hover:underline hover:text-white"
+              >
+                Montreal
+              </a>
+            </li>
+            <li className="my-1.5">
+              <a
+                href="https://www.doordash.com/food-delivery/"
+                className="text-gray-300 text-sm no-underline  font-semibold hover:underline hover:text-white"
+              >
+                All Cities
+              </a>
+            </li>
+          </ul>
         </div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      
+      </div>
+      <div className='md:w-5/12'>
+        <h4 className="text-gray-200 font-bold text-md mb-3">
+          Top Cuisines Near You
+        </h4>
+        <div className='flex justify-between'>
+          <ul className='md:w-1/3'>
+            {" "}
+            <li className="my-1.5">
+              <a
+                href="https://www.doordash.com/cuisine/pizza-near-me/"
+                className="text-gray-300 text-sm no-underline  font-semibold hover:underline hover:text-white"
+              >
+                Pizza Near Me
+              </a>
+            </li>
+            <li className="my-1.5">
+              <a
+                href="https://www.doordash.com/cuisine/chinese-near-me/"
+                className="text-gray-300 text-sm no-underline  font-semibold hover:underline hover:text-white"
+              >
+                Chinese Food Near Me
+              </a>
+            </li>
+            <li className="my-1.5">
+              <a
+                href="https://www.doordash.com/cuisine/sushi-near-me/"
+                className="text-gray-300 text-sm no-underline  font-semibold hover:underline hover:text-white"
+              >
+                Sushi Near Me
+              </a>
+            </li>
+            <li className="my-1.5">
+              <a
+                href="https://www.doordash.com/cuisine/cafe-near-me/"
+                className="text-gray-300 text-sm no-underline  font-semibold hover:underline hover:text-white"
+              >
+                Cafe Near Me
+              </a>
+            </li>
+            <li className="my-1.5">
+              <a
+                href="https://www.doordash.com/cuisine/mexican-near-me/"
+                className="text-gray-300 text-sm no-underline  font-semibold hover:underline hover:text-white"
+              >
+                Mexican Food Near Me
+              </a>
+            </li>
+            <li className="my-1.5">
+              <a
+                href="https://www.doordash.com/cuisine/thai-near-me/"
+                className="text-gray-300 text-sm no-underline  font-semibold hover:underline hover:text-white"
+              >
+                Thai Food Near Me
+              </a>
+            </li>
+            <li className="my-1.5">
+              <a
+                href="https://www.doordash.com/cuisine/lunch-near-me/"
+                className="text-gray-300 text-sm no-underline  font-semibold hover:underline hover:text-white"
+              >
+                Lunch Near Me
+              </a>
+            </li>
+            <li className="my-1.5">
+              <a
+                href="https://www.doordash.com/cuisine/seafood-near-me/"
+                className="text-gray-300 text-sm no-underline  font-semibold hover:underline hover:text-white"
+              >
+                Sea Food Near Me
+              </a>
+            </li>
+          </ul>
+          <ul className='md:w-1/3'>
+            {" "}
+            <li className="my-1.5">
+              <a
+                href="https://www.doordash.com/cuisine/indian-near-me/"
+                className="text-gray-300 text-sm no-underline  font-semibold hover:underline hover:text-white"
+              >
+                Indian Food Near Me
+              </a>
+            </li>
+            <li className="my-1.5">
+              <a
+                href="https://www.doordash.com/cuisine/dessert-near-me/"
+                className="text-gray-300 text-sm no-underline  font-semibold hover:underline hover:text-white"
+              >
+                Dessert Near Me
+              </a>
+            </li>
+            <li className="my-1.5">
+              <a
+                href="https://www.doordash.com/cuisine/burger-near-me/"
+                className="text-gray-300 text-sm no-underline  font-semibold hover:underline hover:text-white"
+              >
+                Burgers Near Me
+              </a>
+            </li>
+            <li className="my-1.5">
+              <a
+                href="https://www.doordash.com/cuisine/asian-near-me/"
+                className="text-gray-300 text-sm no-underline  font-semibold hover:underline hover:text-white"
+              >
+                Asian Food Near Me
+              </a>
+            </li>
+            <li className="my-1.5">
+              <a
+                href="https://www.doordash.com/cuisine/italian-near-me/"
+                className="text-gray-300 text-sm no-underline  font-semibold hover:underline hover:text-white"
+              >
+                Italian Food Near Me
+              </a>
+            </li>
+            <li className="my-1.5">
+              <a
+                href="https://www.doordash.com/cuisine/vegan-near-me/"
+                className="text-gray-300 text-sm no-underline  font-semibold hover:underline hover:text-white"
+              >
+                Vegan Food Near Me
+              </a>
+            </li>
+            <li className="my-1.5">
+              <a
+                href="https://www.doordash.com/cuisine/sandwich-near-me/"
+                className="text-gray-300 text-sm no-underline  font-semibold hover:underline hover:text-white"
+              >
+                Sandwiches Near Me
+              </a>
+            </li>
+            <li className="my-1.5">
+              <a
+                href="https://www.doordash.com/cuisine/restaurants-near-me/"
+                className="text-gray-300 text-sm no-underline  font-semibold hover:underline hover:text-white"
+              >
+                Restaurants Near Me
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
