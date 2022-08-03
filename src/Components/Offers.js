48a52c824fe6c3cb1141762f2c1df0102ed54a3c
Tagbo Chinedu <johntagbo2@gmail.com>
2022-08-03 12:21:48 +0100

    final commit

--- src/Components/Offers.js
+++ src/Components/Offers.js
@@ -18,9 +18,9 @@ const Offers = () => {
               As a delivery driver, you'll make reliable money—working anytime,
               anywhere.
             </p>
-            <div className="flex items-center md:justify-center text-btnred font-semibold">
+            <div className="flex items-center md:justify-center text-btnred font-bold">
               <a href="https://www.doordash.com/dasher/signup/?utm_source=dx_signup_midtile_cx_home">
-                start earning
+                Start earning
               </a>
               <ArrowNarrowRightIcon className="w-3 h-3" />
             </div>
@@ -39,7 +39,7 @@ const Offers = () => {
             <p className="md:text-center font-semibold md:w-10/12 text-lg md:mx-auto">
               Grow your business and reach new customers by partnering with us.
             </p>
-            <div className="flex items-center md:justify-center text-btnred font-semibold">
+            <div className="flex items-center md:justify-center text-btnred font-bold">
               <a href="https://www.doordash.com/dasher/signup/?utm_source=dx_signup_midtile_cx_home">
                 Sign up your store
               </a>
@@ -60,7 +60,7 @@ const Offers = () => {
             <p className="md:text-center font-semibold md:w-10/12 text-lg md:mx-auto">
               Get the best DoorDash experience with live order tracking.
             </p>
-            <div className="flex items-center md:justify-center text-btnred font-semibold">
+            <div className="flex items-center md:justify-center text-btnred font-bold">
               <a
                 href="https://apps.apple.com/app/id719972451?mt=8&pt=9654801&ct=Mobile+Splash+Page+%28New+-+08%2F3%2F17%29"
                 className=""
